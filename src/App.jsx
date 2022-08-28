3c6df2029a87e78ae1c55c880ed74a4b20478691
Saurabh Singh <singhsaurabh162000@gmail.com>
2022-08-28 16:28:46 +0530

    basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import { Routes, Route } from "react-router-dom"
+import { Box } from '@mui/material'
+import Navbar from './Components/Navbar'
+import Home from './Pages/Home'
+import ExerciseDetails from './Pages/ExerciseDetails'
+import Footer from './Components/Footer'
 function App() {
     return (
-        <div>App</div>
+        <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
+            <Navbar />
+            <Routes>
+                <Route path='/' element={<Home />}></Route>
+                <Route path='/exercise/:id' element={<ExerciseDetails />}></Route>
+            </Routes>
+            <Footer />
+        </Box>
     )
 }
 
---
+++ src/Pages/ExerciseDetails.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ExerciseDetails() {
+    return (
+        <div>ExerciseDetails</div>
+    )
+}
+
+export default ExerciseDetails
