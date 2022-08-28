536c0f0c17621c9ec409ffa119016d79675ce48c
Saurabh Singh <singhsaurabh162000@gmail.com>
2022-08-29 01:47:47 +0530

    creating 3 files for perticular exercise

---
+++ src/Components/ExerciseVideos.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ExerciseVideos() {
+    return (
+        <div>ExerciseVideos</div>
+    )
+}
+
+export default ExerciseVideos
---
+++ src/Components/SimilarExercises.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SimilarExercises() {
+    return (
+        <div>SimilarExercises</div>
+    )
+}
+
+export default SimilarExercises
