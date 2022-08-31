3d4000a138c553a6eebdf7f303e0efc70810663e
Saurabh Singh <singhsaurabh162000@gmail.com>
2022-08-31 10:43:17 +0530

    adding some images for background

--- src/Pages/ExerciseDetails.jsx
+++ src/Pages/ExerciseDetails.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { Box } from "@mui/material"
+import { useParam } from "react-router-dom"
+
+import { exerciseOptions, fetchData } from "../UtilityFunction/fetchData"
+import Detail from '../Components/Detail'
+import ExerciseVideos from '../Components/ExerciseVideos'
+import SimilarExercises from '../Components/SimilarExercises'
 
 function ExerciseDetails() {
+    const [exerciseDetail, setExerciseDetail] = React.useState({});
+    // const { id } = useParam();
+
+    // React.useEffect(() => {
+    //     const fetchExerciseData = async () => {
+
+    //     }
+    //     fetchExerciseData();
+    // }, [id])
     return (
-        <div>ExerciseDetails</div>
+        <Box>
+            <Detail />
+            <ExerciseVideos />
+            <SimilarExercises />
+        </Box>
     )
 }
 
