a5d9f1697705dcc8f1164214c6f20cc3cb005e50
Saurabh <singhsaurabh162000@gmail.com>
2022-10-25 17:52:31 +0530

    updating

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client"
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
+import App from './App';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <BrowserRouter><App /></BrowserRouter>
-)
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+);
+
