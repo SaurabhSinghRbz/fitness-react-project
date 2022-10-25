b5860d03037a436c3ebcc8856c1d3dc0ea917826
Saurabh <singhsaurabh162000@gmail.com>
2022-10-25 17:51:28 +0530

    video endpoint added

--- src/UtilityFunction/fetchData.js
+++ src/UtilityFunction/fetchData.js
@@ -1,15 +1,21 @@
 export const exerciseOptions = {
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-        'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY
-    }
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': 'a8a3d1fb6emsh6ddf1f607d6d9e4p1a8264jsn9bb2b1d5e445',
+  },
 };
 
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': 'f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85',
+  },
+};
 
 export const fetchData = async (url, options) => {
-    const response = await fetch(url, options)
-    const data = await response.json();
-    return data;
-
-}
+  const res = await fetch(url, options);
+  const data = await res.json();
+  return data;
+};
