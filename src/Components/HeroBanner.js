2dca81755cfe58c167cb5c6ad94c1b818b10b497
Saurabh <singhsaurabh162000@gmail.com>
2022-10-26 13:47:07 +0530

    updating herobanner

--- src/Components/HeroBanner.js
+++ src/Components/HeroBanner.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Box, Stack, Typography } from '@mui/material';
 
 import HeroBannerImage from '../assets/images/banner.png';
-
+// import BackgoundImage from "../assets/images/2.jpg";
 const HeroBanner = () => (
-  <Box sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px">
-    <Typography color="#FF2625" fontWeight="600" fontSize="26px">Fitness Club</Typography>
-    <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '40px' } }} mb="23px" mt="30px">
-      Sweat, Smile <br />
-      And Repeat
+  <Box sx={{ mt: { lg: '50px', xs: '40px' }, ml: { sm: '50px' } }} position="relative" p="20px">
+    <Typography color="#FF2625" fontWeight="600" fontSize="50px" fontFamily="monospace">Fitness Club</Typography>
+    <Typography fontWeight={400} fontFamily="cursive" sx={{ fontSize: { lg: '35px', xs: '30px' } }} mb="23px" mt="30px">
+      Push through the pain...<br />
+      Every Single Day
     </Typography>
     <Typography fontSize="22px" fontFamily="Alegreya" lineHeight="35px">
       Check out the most effective exercises personalized to you
