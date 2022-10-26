6d19eed6b68603784f655778efea3a55320a6d63
Saurabh <singhsaurabh162000@gmail.com>
2022-10-26 12:20:05 +0530

    navbar updated

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Box, Stack, Typography } from '@mui/material';
-import Logo from '../assets/images/Logo-1.png';
+import Logo from '../assets/images/Logo2.png';
 
 const Footer = () => (
   <Box mt="80px" bgcolor="#FFF3F4">
