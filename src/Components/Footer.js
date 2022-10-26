c57d1149c4abcc2d31e8e1453f6f91c910748e3f
Saurabh <singhsaurabh162000@gmail.com>
2022-10-26 13:02:21 +0530

    footer + logo updated

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Box, Stack, Typography } from '@mui/material';
 import Logo from '../assets/images/Logo2.png';
+import { Link } from 'react-router-dom';
 
 const Footer = () => (
-  <Box mt="80px" bgcolor="#FFF3F4">
-    <Stack sx={{ alignItems: 'center' }} flexWrap="wrap" flex justifyContent="center" pt="20px">
-      <img src={Logo} alt="logo" style={{ width: '200px' }} />
+  <Box mt="80px" boxShadow="rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px">
+    <Stack sx={{ alignItems: 'center' }} flexWrap="wrap" flex justifyContent="center">
+      <Link to="/">
+        <img src={Logo} alt="logo" style={{ width: '220px', margin: "-60px 0" }} />
+      </Link>
     </Stack>
-    <Typography sx={{ fontSize: { lg: '22px', xs: '20px' } }} mt="10px" pb="20px" textAlign="center">Made with ❤️ by Saurabh Singh</Typography>
+    <Typography sx={{ fontSize: { lg: '22px', xs: '20px' } }} pb="22px" textAlign="center" >Made with ❤️ by Saurabh Singh</Typography>
   </Box>
 );
 
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,22 +5,23 @@ import { Stack } from '@mui/material';
 import Logo from '../assets/images/Logo4.png';
 
 const Navbar = () => {
-  return <Stack direction="row" alignContent="center" sx={{ gap: { sm: '100px', xs: '20px' }, justifyContent: { xs: "space-around", sm: "left" }, pl: { xs: "0", sm: "20px" } }} pl="40px" mt="0">
+  return <Stack direction="row" alignContent="center" sx={{ gap: { sm: '50px' }, justifyContent: { xs: "space-around", sm: "left" }, pl: { xs: "0", sm: "20px" } }} pl="40px" mt="0">
     <Link to="/">
-      <img src={Logo} alt="logo" style={{ width: '120px', margin: '0px' }} />
+      <img src={Logo} alt="logo" style={{ width: '110px', margin: '0px' }} />
     </Link>
     <Stack
       direction="row"
       gap="40px"
-      fontFamily="Alegreya"
-      fontSize="24px"
+      fontFamily="sans-serif"
+      fontSize="25px"
+      fontWeight="bold"
       alignItems="center"
+      sx={{ fontSize: { xs: "22px", sm: "25px" }, }}
     >
-      <Link to="/" style={{ textDecoration: 'none', color: '#3A1212', borderBottom: '3px solid #FF2625' }}>Home</Link>
-      <Link to="/exercises" style={{ textDecoration: 'none', color: '#3A1212', borderBottom: '3px solid #FF2625' }}>Exercises</Link>
-      {/* <a href="#exercises" style={{ textDecoration: 'none', color: '#3A1212' }}>Exercises</a> */}
+      <Link to="/" style={{ textDecoration: 'none', color: '#FF2625', borderBottom: '3px solid #FF2625' }}>Home</Link>
+      <a href="#exercises" style={{ textDecoration: 'none', color: '#3A1212' }}>Exercises</a>
     </Stack>
-  </Stack>
+  </Stack >
 };
 
 export default Navbar;
