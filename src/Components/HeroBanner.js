782164db513e79e21cc2cfb2d6207fb822a2008f
Saurabh <singhsaurabh162000@gmail.com>
2022-10-26 22:19:05 +0530

    fixing bug after deploy

--- src/Components/HeroBanner.js
+++ src/Components/HeroBanner.js
@@ -5,7 +5,7 @@ import HeroBannerImage from '../assets/images/banner.jpg';
 // import BackgoundImage from "../assets/images/2.jpg";
 const HeroBanner = () => (
   <Box sx={{ mt: { lg: '50px', xs: '40px' }, ml: { sm: '50px' } }} position="relative" p="20px" >
-    <Typography color="#FF2625" fontWeight="600" fontSize="50px" fontFamily="monospace">Saurabh's <br />Fitness Club</Typography>
+    <Typography color="#FF2625" fontWeight="600" fontSize="50px" fontFamily="sans-serif">Saurabh's <br />Fitness Club</Typography>
     <Typography fontWeight={400} fontFamily="cursive" sx={{ fontSize: { lg: '35px', xs: '30px' } }} mb="23px" mt="30px">
       Push through the pain...<br />
       Every Single Day
