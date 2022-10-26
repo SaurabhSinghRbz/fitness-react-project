44890b823179fe5a810b8f803f7c8bf9331771df
Saurabh <singhsaurabh162000@gmail.com>
2022-10-26 17:24:58 +0530

    exercise card updated

--- src/Components/Exercises.js
+++ src/Components/Exercises.js
@@ -42,7 +42,7 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
   return (
     <Box id="exercises" sx={{ mt: { lg: '109px' } }} mt="50px" p="20px">
       <Typography variant="h4" fontWeight="bold" sx={{ fontSize: { lg: '44px', xs: '30px' } }} mb="46px">Showing Results</Typography>
-      <Stack direction="row" sx={{ gap: { lg: '107px', xs: '50px' } }} flexWrap="wrap" justifyContent="center">
+      <Stack direction="row" gap="60px 50px" flexWrap="wrap" justifyContent="center">
         {currentExercises.map((exercise, idx) => (
           <ExerciseCard key={idx} exercise={exercise} />
         ))}
