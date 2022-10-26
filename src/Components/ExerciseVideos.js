8e60ac5fffbd3f4aca9d5d1734db9e2080514f29
Saurabh <singhsaurabh162000@gmail.com>
2022-10-26 22:11:54 +0530

    fixing issue after deploy

--- src/Components/ExerciseVideos.js
+++ src/Components/ExerciseVideos.js
@@ -21,7 +21,7 @@ const ExerciseVideos = ({ exerciseVideos, name }) => {
           >
             <img style={{ borderTopLeftRadius: '20px' }} src={item.video.thumbnails[0].url} alt={item.video.title} width="100%" height="220px" />
             <Box>
-              <Typography sx={{ fontSize: { lg: '20px', xs: '16px' } }} fontWeight={600} fontFamily="cursive" color="#000">
+              <Typography sx={{ fontSize: { lg: '20px', xs: '16px' } }} fontWeight={700} fontFamily="cursive" color="#000">
                 {item.video.title}
               </Typography>
               <Typography fontSize="15px" color="#000" fontFamily="monospace" fontWeight="400">
