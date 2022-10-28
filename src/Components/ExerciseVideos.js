7a29a68ee0f3cece799ba5dcc25a0ab4b643b683
Saurabh <singhsaurabh162000@gmail.com>
2022-10-28 15:51:06 +0530

    fixing loader bug with related videos

--- src/Components/ExerciseVideos.js
+++ src/Components/ExerciseVideos.js
@@ -3,47 +3,31 @@ import { Typography, Box, Stack } from '@mui/material';
 import Loader from './Loader';
 
 const ExerciseVideos = ({ exerciseVideos, name }) => {
-  if (!exerciseVideos.length) return <Loader />;
 
   return (
     <Box sx={{ marginTop: { lg: '80px', xs: '20px' } }} p="20px">
       <Typography sx={{ fontSize: { lg: '44px', xs: '25px' } }} fontWeight={700} color="#000" mb="33px">
         Watch <span style={{ color: '#FF2625', textTransform: 'capitalize' }}>{name}</span> exercise videos
       </Typography>
-      <Stack direction="row" gap="50px" flexWrap="wrap" justifyContent="center">
-        {exerciseVideos?.slice(0, 9)?.map((item, index) => (
-          // <a
-          //   key={index}
-          //   className="exercise-video"
-          //   href={`https://www.youtube.com/watch?v=${item.video.videoId}`}
-          //   target="_blank"
-          //   rel="noreferrer"
-          // >
-          //   <img style={{ borderTopLeftRadius: '20px' }} src={item.video.thumbnails[0].url} alt={item.video.title} width="100%" height="220px" />
-          //   <Box>
-          //     <Typography sx={{ fontSize: { lg: '20px', xs: '16px' } }} fontWeight={700} fontFamily="cursive" color="#000">
-          //       {item.video.title}
-          //     </Typography>
-          //     <Typography fontSize="15px" color="#000" fontFamily="monospace" fontWeight="400">
-          //       {item.video.channelName}
-          //     </Typography>
-          //   </Box>
-          // </a>
-          <Box key={index} className="exercise-video">
-            <iframe width="100%" height="200px" style={{ borderRadius: "25px 0 0 0" }} title="YouTube video player" src={`https://www.youtube.com/embed/${item.video.videoId}`} allowFullScreen />
-            <a href={`https://www.youtube.com/watch?v=${item.video.videoId}`} target="_blank" rel="noreferrer">
-              <Typography sx={{ fontSize: { lg: '20px', xs: '16px' } }} fontWeight={700} fontFamily="cursive" color="#000">
-                {item.video.title}
-              </Typography>
-            </a>
-            <a href={`https://www.youtube.com/channel/${item.video.channelId}`} target="_blank" rel="noreferrer">
-              <Typography fontSize="15px" color="#000" fontFamily="monospace" fontWeight="400">
-                {item.video.channelName}
-              </Typography>
-            </a>
-          </Box>
-        ))}
-      </Stack>
+      {!exerciseVideos.length ? <Loader /> : (
+        <Stack direction="row" gap="50px" flexWrap="wrap" justifyContent="center">
+          {exerciseVideos?.slice(0, 9)?.map((item, index) => (
+            <Box key={index} className="exercise-video">
+              <iframe width="100%" height="200px" style={{ borderRadius: "25px 0 0 0" }} title="YouTube video player" src={`https://www.youtube.com/embed/${item.video.videoId}`} allowFullScreen />
+              <a href={`https://www.youtube.com/watch?v=${item.video.videoId}`} target="_blank" rel="noreferrer">
+                <Typography sx={{ fontSize: { lg: '20px', xs: '16px' } }} fontWeight={700} fontFamily="cursive" color="#000">
+                  {item.video.title}
+                </Typography>
+              </a>
+              <a href={`https://www.youtube.com/channel/${item.video.channelId}`} target="_blank" rel="noreferrer">
+                <Typography fontSize="15px" color="#000" fontFamily="monospace" fontWeight="400">
+                  {item.video.channelName}
+                </Typography>
+              </a>
+            </Box>
+          ))}
+        </Stack>
+      )}
     </Box>
   );
 };
