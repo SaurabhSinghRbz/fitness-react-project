73808e01ebd1b3bdfe0ee686fc80da5a9b0c0053
Saurabh <singhsaurabh162000@gmail.com>
2023-12-17 00:11:23 +0530

    added instructions for all exercises

--- src/Components/Detail.js
+++ src/Components/Detail.js
@@ -6,7 +6,10 @@ import TargetImage from '../assets/icons/target.png';
 import EquipmentImage from '../assets/icons/equipment.png';
 
 const Detail = ({ exerciseDetail }) => {
-  const { bodyPart, gifUrl, name, target, equipment } = exerciseDetail;
+  const { bodyPart, gifUrl, name, target, equipment, instructions, secondaryMuscles } = exerciseDetail;
+
+  let targetMuscles = secondaryMuscles ? [target, ...secondaryMuscles] : [target];
+  targetMuscles = targetMuscles.join(', ');
   const extraDetail = [
     {
       icon: BodyPartImage,
@@ -14,7 +17,7 @@ const Detail = ({ exerciseDetail }) => {
     },
     {
       icon: TargetImage,
-      name: target,
+      name: targetMuscles,
     },
     {
       icon: EquipmentImage,
@@ -26,15 +29,24 @@ const Detail = ({ exerciseDetail }) => {
     <Stack gap="60px" sx={{ flexDirection: { lg: 'row' }, p: '0 20px', alignItems: 'center' }}>
       <img src={gifUrl} alt={name} loading="lazy" className="detail-image" />
       <Stack sx={{ gap: { lg: '35px', xs: '20px' } }}>
-        <Typography sx={{ fontSize: { lg: '64px', xs: '30px' } }} fontWeight={700} textTransform="capitalize">
+        <Typography sx={{ fontSize: { lg: '56px', xs: '25px' } }} fontWeight={700} textTransform="capitalize">
           {name}
         </Typography>
-        <Typography sx={{ fontSize: { lg: '24px', xs: '18px' } }} color="#4F4C4C">
+        <Typography sx={{ fontSize: { lg: '40px', xs: '22px' } }} fontWeight={400} textTransform="capitalize">
+          Instructions :
+        </Typography>
+        {/* <Typography sx={{ fontSize: { lg: '24px', xs: '18px' } }} color="#4F4C4C">
           Exercises keep you strong.{' '}
           <span style={{ textTransform: 'capitalize' }}>{name}</span> bup is one
           of the best <br /> exercises to target your {target}. It will help you improve your{' '}
           <br /> mood and gain energy.
-        </Typography>
+        </Typography> */}
+        {/* Loop through instructions array and display each instruction with a number */}
+        {instructions?.map((instruction, index) => (
+          <Typography key={index} sx={{ fontSize: { lg: '20px', xs: '15px' } }} color="#4F4C4C">
+            <span style={{ fontWeight: 400, color: '#FF2625' }}>{index + 1}.</span> {instruction}
+          </Typography>
+        ))}
         {extraDetail?.map((item) => (
           <Stack key={item.name} direction="row" gap="24px" alignItems="center">
             <Button sx={{ background: '#FFF2DB', borderRadius: '50%', width: '100px', height: '100px' }}>
